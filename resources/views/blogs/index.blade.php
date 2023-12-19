dd3e65c02b94546f20c3a49432c2bfc0b51f1bb5
Kei-K23 <keiksl2301@gmail.com>
2023-12-19 19:24:19 +0630

    add blogs route

---
+++ resources/views/blogs/index.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('Blogs') }}
+        </h2>
+    </x-slot>
+
+    <div>
+        You are inside blogs
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('blogs.index');
+})->name('blogs');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// profile routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
